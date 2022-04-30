4bb1973ef0ee5fc1e9ec8a08a7dd9bc29acd6218
hyperdarker <hyperdarker@gmail.com>
2022-04-30 14:46:37 -0500

    remove chrono.sh and modify tracker.sh

--- tracker.sh
+++ tracker.sh
@@ -1,41 +1,50 @@
 #!/bin/bash
+MINUTES=180 # 3 hours expected
 FILE="$HOME/.log/tracker"
-if [[ "$1" == "log" ]]
-then
-    tail -6 "$FILE"
+
+if [[ "$1" == "log" ]]; then
+    cat "$FILE"
     exit 0
 fi
 
-TYPE=""
-MINUTES=0
-
-config() {
-    TYPE=$1
-    MINUTES=$2
-}
-
-if [ $# -eq 0 ];then
-    config "D" 15
-
-elif [ "$1" == "a" ];then
-    config "A" 30
-
-elif [ "$1" == "b" ];then
-    config "B" 20
-
-else
-    config "X" $1
-    re='^[0-9]+$'
+if [ $# -gt 0 ];then
+    MINUTES=$1
+    re='^[0-9.]+$'
     if ! [[ $MINUTES =~ $re ]] ; then
         echo "error: Not a number" >&2; exit 1
     fi
 fi
 
-START_TIME=$(date '+%X %D')
-echo -e "[$MINUTES min]  $TYPE"
-sleep $((MINUTES*60))
-#sleep $((MINUTES*1)) # for testing
-STOP_TIME=$(date '+%X %D')
-echo -e "\nFrom\t" $START_TIME "\nTo\t" $STOP_TIME
-echo -e "[$MINUTES min]\t$TYPE\t" $START_TIME ">" $STOP_TIME >> $FILE
-notify-send -u critical "SESION FINALIZADA!"
+elapsed=0
+start=$(date +%s)
+STATUS="RUNNING"
+
+while true; do 
+    #read  -r -p  "[Activa] [p] Pausar, [f] Finalizar: "  answer
+    if [ $STATUS == "RUNNING" ]; then
+        start=$(date +%s)
+        read -p $'\e[32m[RUNNING]\e[0m ENTER to Pause. ';
+        stop=$(date +%s)
+        elapsed=$((elapsed + stop - start))
+        STATUS="PAUSE"
+    elif [ $STATUS == "PAUSE" ]; then
+        echo   " Elapsed:" $(date -d@$elapsed -u +%H:%M:%S)
+        echo -e "Expected:" $(date -d@$((MINUTES*60)) -u +%H:%M:%S)
+        while true; do
+            read -p $'\e[35m[PAUSED]\e[0m R to Resume or S to Stop. ' answer
+            if [ -z $answer ]; then continue; fi
+            if [ $answer == 'r' ] || [ $answer == 'R' ]; then
+                STATUS="RUNNING"
+                break
+            elif [ $answer == 's' ] || [ $answer == 'S' ]; then
+                if [ $elapsed -ge 120 ]; then
+                    echo "Elapsed: ("$(date -d@$elapsed -u +%H:%M:%S)") at" $(date '+%H:%M %D')" ...SAVED!"
+                    echo " "$(date -d@$elapsed -u +%H:%M:%S)" at" $(date '+%H:%M %D') >> $FILE
+                else
+                    echo "Don't Saved."
+                fi
+                exit 0
+            fi
+        done
+    fi    
+done
